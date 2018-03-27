f6fc9aa54ea8816ca79e40b5eb102ce1228dada0
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-03-27 01:21:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,12 +4,12 @@ var playlist = {
   sharonVanEtten: 'oneDay'
 }
 
-function updatePlaylist(playlist, artist, song) {
- playlist[artist] = 'song'; 
+function updatePlaylist(playlist, artistName, song) {
+ playlist[artistName] = 'song'; 
  return playlist
 }
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
   return playlist
 }
