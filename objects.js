491bc933d5abcb740cc1aeec1fe6b4575de6059d
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-03-27 01:23:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,6 +10,6 @@ function updatePlaylist(playlist, artistName, song) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+  delete playlist[artistName]
   return playlist
 }
